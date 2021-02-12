67b74616d2e8cfdd44c0bf6eefe7e736e467de67
bconfessor <brian_confessor@poli.ufrj.br>
2021-02-12 02:31:07 -0300

    Criação da base dos componentes de Home e Erro 404
    - Criação da base dos componentes de Home e Erro404
    - Configuração das rotas dos componentes em app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AreaAdminComponent } from './area-admin/area-admin.component';
+import { Error404Component } from './error404/error404.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
+    path: '',
+    component: HomeComponent
+  },
+  {
     path: 'admin',
     component: AreaAdminComponent
-  }
+  },
+  {
+    path: '**',
+    component: Error404Component
+    
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AreaAdminModule } from './area-admin/area-admin.module';
+import { HomeComponent } from './home/home.component';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
@@ -16,7 +20,13 @@ import { AreaAdminModule } from './area-admin/area-admin.module';
     BrowserAnimationsModule,
     AreaAdminModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt'
+  }, {
+    provide: DEFAULT_CURRENCY_CODE,
+    useValue: 'BRL'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
