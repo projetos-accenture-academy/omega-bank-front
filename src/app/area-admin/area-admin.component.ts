bcc250d30c23c32da31350baa5f522704be81e6f
kellisonjk <kellisonjk@gmail.com>
2021-02-12 09:55:03 -0300

    Corrigindo posições do menu e inserindo headers default. Finalização da base.

--- src/app/area-admin/area-admin.component.ts
+++ src/app/area-admin/area-admin.component.ts
@@ -1,6 +1,7 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatDrawerMode } from '@angular/material/sidenav';
+import { MenuItem } from 'src/types/menu-item';
 
 @Component({
   selector: 'app-area-admin',
@@ -11,6 +12,28 @@ export class AreaAdminComponent implements OnInit {
   @ViewChild('sidenav') elementView: ElementRef | undefined;
 
   mode: MatDrawerMode = 'side';
+
+  menuitems: MenuItem[] = [{
+    title: 'Dashboard',
+    routerLink: 'dashboard',
+    tooltip: 'Dashboard com resumos',
+    icon: 'dashboard'
+  }, {
+    title: 'Plano de contas',
+    routerLink: 'planos-conta',
+    tooltip: 'Cadastro de planos de contas',
+    icon: 'category'
+  }, {
+    title: 'Meu perfil',
+    routerLink: 'meu-perfil',
+    tooltip: 'Dados de cadastro',
+    icon: 'account_circle'
+  }, {
+    title: 'Extratos',
+    routerLink: '#',
+    tooltip: 'Extrato de lançamentos de contas',
+    icon: 'summarize'
+  }]
 
   constructor(private breakpointObserver: BreakpointObserver) {
 
---
+++ src/types/menu-item.ts
@@ -0,0 +1,6 @@
+export interface MenuItem {
+  title: string,
+  routerLink: string,
+  tooltip: string,
+  icon: string
+}
