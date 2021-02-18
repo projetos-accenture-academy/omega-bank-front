f896c5591e8273c5426bf57ac562eeeb8b9e77e3
kellisonjk <kellisonjk@gmail.com>
2021-02-18 17:59:17 -0300

    Final real

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
 
-  //API_URL: "https://omegabank-backend.herokuapp.com/"
-  API_URL: "http://localhost:8080/"
+  API_URL: "https://omegabank-backend.herokuapp.com/"
+  //API_URL: "http://localhost:8080/"
 };
 
